61c29f7b4838a57bb44e4efbeb7b7e09562ff67c
shota182 <104834704+shota182@users.noreply.github.com>
2023-11-22 17:14:55 +0900

    Update imu_ekf.cpp

--- imu_ekf.cpp
+++ imu_ekf.cpp
@@ -1,1 +1,112 @@
-// cpp
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/MagneticField.h>
+#include <geometry_msgs/Vector3.h>
+#include <fstream>
+#include <cmath>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+const double DEG2RAD = M_PI / 180.0;
+
+class ErrorEllipse {
+public:
+    ErrorEllipse(double confidence_interval) : confidence_interval(confidence_interval) {}
+
+    void calcErrorEllipse(const Matrix2d& Pxy, double& x, double& y, double& ang_rad) {
+        // Implementation of error ellipse calculation (replace with your implementation)
+        // ...
+
+        // Placeholder values (replace with actual calculation)
+        x = 0.1;
+        y = 0.1;
+        ang_rad = 45.0 * DEG2RAD;
+    }
+
+private:
+    double confidence_interval;
+};
+
+class ExtendedKalmanFilter {
+public:
+    ExtendedKalmanFilter(double period_ms) : dts(period_ms / 1000.0) {
+        // Initialize your variables here
+        // ...
+    }
+
+    void getEKF() {
+        // Implementation of EKF (replace with your implementation)
+        // ...
+
+        // Placeholder values (replace with actual calculation)
+        // x_true, y, x_hat, P
+        MatrixXd x_true(2, 1), y(2, 1), x_hat(2, 1);
+        Matrix2d P;
+        // ...
+
+        // Print results (replace with your desired output)
+        ROS_INFO_STREAM("x_true:\n" << x_true << "\ny:\n" << y << "\nx_hat:\n" << x_hat << "\nP:\n" << P);
+    }
+
+private:
+    double dts;
+    // Add your other member variables and functions here
+    // ...
+};
+
+void imuRawCallback(const sensor_msgs::Imu::ConstPtr& msg) {
+    // Implementation of IMU data processing (replace with your implementation)
+    // ...
+
+    // Placeholder: Print received IMU data
+    ROS_INFO_STREAM("Received IMU Raw Data");
+}
+
+void imuMagCallback(const sensor_msgs::MagneticField::ConstPtr& msg) {
+    // Implementation of magnetometer data processing (replace with your implementation)
+    // ...
+
+    // Placeholder: Print received magnetometer data
+    ROS_INFO_STREAM("Received Magnetometer Data");
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "imu_ekf");
+    ros::NodeHandle nh;
+
+    const double confidence_interval = 99.0;
+
+    double period_ms = 100.0;
+    int frame_cnt = static_cast<int>(12000.0 / period_ms);
+
+    ExtendedKalmanFilter ekf(period_ms);
+    ErrorEllipse ee(confidence_interval);
+
+    // Open output file
+    std::ofstream outputFile("output_data.txt");
+    if (!outputFile.is_open()) {
+        ROS_ERROR_STREAM("Error opening output file.");
+        return 1;
+    }
+
+    // Subscriber for IMU data_raw
+    ros::Subscriber imuRawSub = nh.subscribe("/imu/data_raw", 1, imuRawCallback);
+
+    // Subscriber for magnetometer data
+    ros::Subscriber imuMagSub = nh.subscribe("/imu/mag", 1, imuMagCallback);
+
+    // Publisher for EKF data
+    ros::Publisher ekfDataPub = nh.advertise<std_msgs::String>("/imu/data_ekf", 1);
+
+    // Call the animation function
+    animate(frame_cnt, ekf, period_ms, ee, outputFile);
+
+    // Close the output file
+    outputFile.close();
+
+    ros::shutdown();
+
+    return 0;
+}
